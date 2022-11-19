05cab36c83631ce9a3ffb5a344f97c1b141f7293
Andrei Kazheunikau <Andrei_Kazheunikau@epam.com>
2022-11-19 20:56:23 +0100

    [module6]: added part2

--- Module6.DataStructures/Tasks/HybridFlowProcessor.cs
+++ Module6.DataStructures/Tasks/HybridFlowProcessor.cs
@@ -5,24 +5,38 @@ namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private readonly DoublyLinkedList<T> _list = new DoublyLinkedList<T>();
+
+        private bool IsEmpty => _list.Length == 0;
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("nothing to Dequeue");
+            }
+
+            return _list.RemoveAt(0);
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            _list.Add(item);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("nothing to Pop");
+            }
+
+            return _list.RemoveAt(_list.Length - 1);
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _list.AddAt(0, item);
         }
     }
 }
