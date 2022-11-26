be9682bc57944b81531ccf2f5928095162e153e7
Andrei Kazheunikau <Andrei_Kazheunikau@epam.com>
2022-11-26 15:07:20 +0100

    make test pass
    method passes the test to return numbers from 1 to 100

--- Module7.UnitTesting/FizzBuzz/Task/Kata.cs
+++ Module7.UnitTesting/FizzBuzz/Task/Kata.cs
@@ -4,6 +4,13 @@ public static class Kata
 {
     public static string[] FizzBuzz()
     {
-        throw new NotImplementedException();
+        var arr = new string[100];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = (i + 1).ToString();
+        }
+
+        return arr;
     }
 }
