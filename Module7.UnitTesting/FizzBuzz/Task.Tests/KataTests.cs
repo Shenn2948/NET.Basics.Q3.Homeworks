0fd4f4ded62cadcc72bb3c0f40b4d88466b863a2
Andrei Kazheunikau <Andrei_Kazheunikau@epam.com>
2022-11-26 16:06:36 +0100

    fix test to return numbers, added test for fizz

--- Module7.UnitTesting/FizzBuzz/Task.Tests/KataTests.cs
+++ Module7.UnitTesting/FizzBuzz/Task.Tests/KataTests.cs
@@ -5,13 +5,36 @@ public class KataTests
     [Fact]
     public void Returns_Numbers_From_1_to_100()
     {
-        // Arrange
-        string[] expected = Enumerable.Range(1, 100).Select(num => num.ToString()).ToArray();
-
-        // Act
         string[] actual = Kata.FizzBuzz();
 
-        // Assert
-        Assert.Equal(expected, actual);
+        foreach (string item in actual)
+        {
+            bool isNumber = int.TryParse(item, out int result);
+
+            if (isNumber)
+            {
+                Assert.InRange(result, 1, 100);
+            }
+        }
+    }
+
+    [Fact]
+    public void Returns_Fizz_InsteadOfNumber_Which_Is_Divisible_By_3()
+    {
+        var expected = new string[] { "1", "2", "Fizz", "4", "5", "Fizz" };
+
+        string[] actual = Kata.FizzBuzz();
+
+        Assert.Equal(expected, actual[0..6]);
+    }
+
+    [Fact]
+    public void Returns_Buzz_InsteadOfNumber_Which_Is_Divisible_By_5()
+    {
+    }
+
+    [Fact]
+    public void Returns_FizzBuzz_InsteadOfNumber_Which_Is_Divisible_By_Both_3_And_5()
+    {
     }
 }
