f274bf1cf097c2814edad29b8e944e68e1e5bb3a
Andrei Kazheunikau <Andrei_Kazheunikau@epam.com>
2022-12-04 18:26:10 +0100

    [module8]: fix add test script to git

--- Module8.SQLFundamentals/SQLProject/dbo/Scripts/test_spEmployee_InsertInfo.sql
+++ Module8.SQLFundamentals/SQLProject/dbo/Scripts/test_spEmployee_InsertInfo.sql
@@ -4,7 +4,7 @@ EXEC [dbo].[spEmployee_InsertInfo]
 	@EmployeeName = 'Kevin Smith',
 	@FirstName = 'Kevin',
 	@LastName = 'Smith',
-	@CompanyName  = 'Company ABC',
+	@CompanyName  = 'ABC Company',
 	@Position = 'manager',
 	@Street  = '1 Street',
 	@City  = 'Washington',
@@ -12,9 +12,9 @@ EXEC [dbo].[spEmployee_InsertInfo]
 	@ZipCode = '444'
 
 IF EXISTS (SELECT TOP (1) * FROM dbo.Person WHERE [FirstName] = 'Kevin')
-   PRINT ('Employee with name Kevin was created')
+   PRINT ('Success - insert Employee')
 
-IF EXISTS (SELECT TOP (1) * FROM dbo.Company c WHERE c.Name = 'Company ABC')
-   PRINT ('Company was created')
+IF EXISTS (SELECT TOP (1) c.Name FROM dbo.Company c WHERE c.Name  = 'ABC Company')
+   PRINT ('Success - insert Company')
 
 ROLLBACK TRANSACTION
