46141234f9e44b358d959eccde9110d925567e78
Andrei Kazheunikau <Andrei_Kazheunikau@epam.com>
2022-12-04 16:34:01 +0100

    [module8]: add task2 implementation

---
+++ Module8.SQLFundamentals/SQLProject/dbo/Views/EmployeeInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+AS 
+SELECT e.Id AS 'EmployeeId'
+	  ,ISNULL(e.EmployeeName, CONCAT(p.FirstName, ' ', p.LastName)) AS 'EmployeeFullName'
+	  ,CONCAT(a.ZipCode, '_', a.State, ', ', a.City, '-', a.Street) AS 'EmployeeFullAddress'
+	  ,CONCAT(e.CompanyName, '(', e.Position, ')') AS 'EmployeeCompanyInfo'
+FROM [dbo].[Employee] e
+INNER JOIN [Person] p ON (e.PersonId = p.Id)
+INNER JOIN [Address] a ON (e.AddressId = a.Id)
+ORDER BY e.CompanyName, a.City ASC
+OFFSET 0 ROWS
