ce9c4123368b09b0999907073223c8f21ea3f446
Andrei Kazheunikau <Andrei_Kazheunikau@epam.com>
2022-12-04 17:32:09 +0100

    [module8]: add task 3 implementation

---
+++ Module8.SQLFundamentals/SQLProject/dbo/Stored Procedures/spEmployee_InsertInfo.sql
@@ -0,0 +1,37 @@
+﻿CREATE PROCEDURE [dbo].[spEmployee_InsertInfo]
+	@EmployeeName nvarchar(100) = NULL, 
+	@FirstName nvarchar(50) = NULL,
+	@LastName nvarchar(50) = NULL,
+	@CompanyName nvarchar(20) = NULL,
+	@Position nvarchar(50) = NULL,
+	@Street nvarchar(50) = NULL,
+	@City nvarchar(20) = NULL,
+	@State nvarchar(50) = NULL,
+	@ZipCode nvarchar(50) = NULL
+AS
+BEGIN
+	IF(RTRIM(ISNULL(@EmployeeName, '')) LIKE '')
+	BEGIN
+		RAISERROR ('Invalid parameter: @EmployeeName cannot be null, empty or whitespace', 16, 1)
+		RETURN
+	END
+
+	IF(LEN(@CompanyName) > 20)
+	BEGIN
+		SET @CompanyName = LEFT(@CompanyName, 20);
+	END
+
+	DECLARE @AddressId INT;
+	DECLARE @PersonId INT;
+
+	INSERT INTO Address(Street, City, State, ZipCode) VALUES(@Street, @City, @State, @ZipCode)
+	SET @AddressId = @@IDENTITY;
+
+	INSERT INTO Person(FirstName, LastName) VALUES(@FirstName, @LastName)
+	SET @PersonId = @@IDENTITY;
+
+	INSERT INTO Company(Name, AddressId) VALUES(@CompanyName, @AddressId)
+
+	INSERT INTO Employee(AddressId, PersonId, CompanyName, Position, EmployeeName)
+	VALUES(@AddressId, @PersonId, @CompanyName, @Position, @EmployeeName);
+END
---
+++ Module8.SQLFundamentals/SQLProject/dbo/Stored Procedures/spEmployee_InsertInfo.test.sql
@@ -0,0 +1,20 @@
+﻿USE [DemoDB]
+GO
+
+BEGIN TRANSACTION  
+
+EXEC [dbo].[spEmployee_InsertInfo]
+	@EmployeeName = 'Kevin Smith',
+	@FirstName = 'Kevin',
+	@LastName = 'Smith',
+	@CompanyName  = 'Long company name, greater than 20 symbols. It should be truncated',
+	@Position = 'manager',
+	@Street  = '1 Street',
+	@City  = 'Washington',
+	@State  = 'Washington',
+	@ZipCode = '444'
+
+IF EXISTS (SELECT TOP (1) * FROM dbo.Person WHERE [FirstName] = 'Kevin')
+   PRINT ('Success')
+
+ROLLBACK TRANSACTION
