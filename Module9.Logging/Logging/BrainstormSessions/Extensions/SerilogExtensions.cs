6644e299ef0e2205f3c69063674d9303e8633d10
Andrei Kazheunikau <Andrei_Kazheunikau@epam.com>
2022-12-13 20:28:20 +0100

    [module9]: add task2

---
+++ Module9.Logging/Logging/BrainstormSessions/Extensions/SerilogExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Serilog.Configuration;
+using Serilog.Events;
+using Serilog.Sinks.Email;
+using Serilog;
+using System.Net;
+
+namespace BrainstormSessions.Extensions
+{
+    public static class SerilogCustomEmailExtension
+    {
+        const string DefaultOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Exception}";
+
+        public static LoggerConfiguration CustomEmail(
+            this LoggerSinkConfiguration loggerConfiguration,
+            CustomEmailConnectionInfo connectionInfo,
+            string outputTemplate = DefaultOutputTemplate,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum
+        )
+        {
+            return loggerConfiguration.Email(connectionInfo, outputTemplate, restrictedToMinimumLevel);
+        }
+
+        public class CustomEmailConnectionInfo : EmailConnectionInfo
+        {
+            public CustomEmailConnectionInfo()
+            {
+                NetworkCredentials = new NetworkCredential();
+            }
+        }
+    }
+}
