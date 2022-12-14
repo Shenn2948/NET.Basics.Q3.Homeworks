662c506282ede3c35d23b6590ee905e80fb34d98
Andrei Kazheunikau <Andrei_Kazheunikau@epam.com>
2022-12-14 17:00:08 +0100

    [module10]: add task2

---
+++ Module10.Serialization/Task2/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization;
+using System.Text;
+
+namespace Task2.Models;
+
+[Serializable]
+public class Person : ISerializable
+{
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+
+    public Person() { }
+
+    public Person(SerializationInfo info, StreamingContext context)
+    {
+        Name = info.GetValue("name", typeof(string)) as string;
+        LastName = info.GetValue("lastName", typeof(string)) as string;
+    }
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        info.AddValue("name", Name);
+        info.AddValue("lastName", LastName);
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"Name: {Name}");
+        sb.AppendLine($"lastName: {LastName}");
+        sb.AppendLine();
+
+        return sb.ToString();
+    }
+}
--- Module10.Serialization/Task2/Program.cs
+++ Module10.Serialization/Task2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+using Task2.Models;
+
+const string FileName = "person.bin";
+
+var person = new Person { Name = "Jhon", LastName = "Carter" };
+
+Console.WriteLine("before");
+Console.WriteLine(person);
+
+Serialize(person);
+var deserialized = Deserialize();
+
+Console.WriteLine("after");
+Console.WriteLine(deserialized);
+
+Console.ReadLine();
+
+static void Serialize(Person person)
+{
+    using FileStream createStream = File.Create(FileName);
+    IFormatter formatter = new BinaryFormatter();
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+    formatter.Serialize(createStream, person);
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+}
+
+static Person? Deserialize()
+{
+    using FileStream openStream = File.OpenRead(FileName);
+    IFormatter formatter = new BinaryFormatter();
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+    return formatter.Deserialize(openStream) as Person;
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+}
